a541f2f3530a533942a678c20675199d2267e560
Raul <raul.muresan01@gmail.com>
2017-10-09 22:37:00 +0300

    deleted unused import

--- lab1/src/main/java/Main.java
+++ lab1/src/main/java/Main.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        String a = scanner.next();
-        System.out.println(a);*/
         Calculator calculator = new Calculator();
         calculator.calculate("1 + 2");
         calculator.calculate("max 2 1");
